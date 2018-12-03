8273d07e8c48e03f3490b7c5ff8420c5eff3f659
qinyue <530495973@qq.com>
2018-12-03 16:26:43 +0800

    多线程求和

--- demo/src/com/qy/testThread/ThreadAndRunnable.java
+++ demo/src/com/qy/testThread/ThreadAndRunnable.java
@@ -1,5 +1,30 @@
 package com.qy.testThread;
 
 public class ThreadAndRunnable {
-
+	public static volatile int sum = 0;
+	public static volatile int count = 1;
+	public static void main(String[] args) {
+		Runnable r1 = new Runnable() {
+			@Override
+			public void run() {
+				synchronized ((Object) count) {
+					// TODO Auto-generated method stub
+					for(int i = 0 ; i < 25 ; i++) {
+						sum+=count;
+						System.out.println("sum= " + sum);
+						count++;
+					}
+				}
+				
+			}
+		};
+		Thread t1 = new Thread(r1);
+		Thread t2 = new Thread(r1);
+		Thread t3 = new Thread(r1);
+		Thread t4 = new Thread(r1);
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+	}
 }
