945ba8302666ce9dc2bf9deb7cad10b477925da9
qinyue530 <530495973@qq.com>
2018-12-03 11:48:26 +0800

    添加线程例子

---
+++ demo/src/com/qy/testThread/OnlyThread.java
@@ -0,0 +1,57 @@
+package com.qy.testThread;
+
+public class OnlyThread {
+	public static int sum = 0;
+	public static int count = 1;
+	public static void main(String[] args) {
+		
+		final Thread t1 = new Thread() {
+			public void run() {
+				for(int i = 0 ; i < 25 ; i++) {
+					sum+=count;
+					System.out.println("t1.sum ========" + sum);
+					count++;
+				}
+			}
+		};
+		
+
+		Thread t2 = new Thread() {
+			public void run() {
+				for(int i = 0 ; i < 25 ; i++) {
+					sum+=count;
+					System.out.println("t2.sum ========" + sum);
+					count++;
+				}
+			}
+		};
+		
+
+		Thread t3 = new Thread() {
+			public void run() {
+				for(int i = 0 ; i < 25 ; i++) {
+					sum+=count;
+					System.out.println("t3.sum ========" + sum);
+					count++;
+				}
+			}
+		};
+		
+
+		Thread t4 = new Thread() {
+			public void run() {
+				for(int i = 0 ; i < 25 ; i++) {
+					sum+=count;
+					System.out.println("t4.sum ========" + sum);
+					count++;
+				}
+			}
+		};
+		
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+	}
+}
---
+++ demo/src/com/qy/testThread/ThreadAndRunnable.java
@@ -0,0 +1,5 @@
+package com.qy.testThread;
+
+public class ThreadAndRunnable {
+
+}
