b4ec9523bfbd5e105bc667466601c7a1fd622a17
qinyue530 <530495973@qq.com>
2018-12-13 21:24:41 +0800

    单例模式 : 懒汉和饿汉

---
+++ demo/src/com/designPattern/single/Slacker.java
@@ -0,0 +1,17 @@
+package com.designPattern.single;
+/**
+ * 懒汉式 单例 ， 调用效率不高 ， 可以延时加载
+ * @author qin_Yue
+ *
+ */
+public class Slacker {
+	private static Slacker instance ;
+	private Slacker() {}
+	public synchronized Slacker getSlacker() {
+		if(instance == null) {
+			instance = new Slacker();
+		}
+		return instance;
+	}
+	
+}
