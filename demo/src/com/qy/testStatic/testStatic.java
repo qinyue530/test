06b775983b8b4877e3cf356cfd604a595aa3d762
qinyue530 <530495973@qq.com>
2018-12-18 10:32:56 +0800

    no message

---
+++ demo/src/com/qy/testStatic/testStatic.java
@@ -0,0 +1,16 @@
+package com.qy.testStatic;
+
+public class testStatic {
+	private static int x = 100;
+
+	public static void main(String args[]) {
+		testStatic hs1 = new testStatic();
+		hs1.x++;
+		testStatic hs2 = new testStatic();
+		hs2.x++;
+		hs1 = new testStatic();
+		hs1.x++;
+		testStatic.x--;
+		System.out.println("x=" + x);
+	}
+}
