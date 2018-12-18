ee124cfb0a05074b3f0359a7ccac8caeb2879a3b
qinyue530 <530495973@qq.com>
2018-12-18 11:22:09 +0800

    volatile 和 AtomicInteger 多线程数数

---
+++ demo/src/com/qy/testThread/JUCThread.java
@@ -0,0 +1,28 @@
+package com.qy.testThread;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class JUCThread {
+	private static volatile AtomicInteger count = new AtomicInteger(0);
+	public static void main(String[] args) {
+		Runnable a = new Runnable() {
+			@Override
+			public void run() {
+				for(int i = 0 ; i < 25 ; i++) {
+					count.incrementAndGet();
+					System.out.println(count);
+				}
+			}
+		};
+		
+		Thread t1 = new Thread(a);
+		Thread t2 = new Thread(a);
+		Thread t3 = new Thread(a);
+		Thread t4 = new Thread(a);
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		
+	}
+}
