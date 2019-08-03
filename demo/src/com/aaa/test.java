c71f78d1e0d37187073975a43cd61c4d47d52948
qinyue530 <530495973@qq.com>
2019-08-03 10:35:54 +0800

    试用贮藏

--- demo/src/com/aaa/test.java
+++ demo/src/com/aaa/test.java
@@ -1,7 +1,18 @@
 package com.aaa;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class test {
+	private static AtomicInteger atomic = new AtomicInteger(0001);
+	private static String time;
 	public static void main(String[] args) {
-		System.out.println('1'+2);
+		String str = null;
+		System.out.println(atomic.getAndIncrement());
+		if(time.equals(String.valueOf(System.currentTimeMillis())))
+			 str = String.valueOf(atomic) + String.valueOf(System.currentTimeMillis());
+		System.out.println(str);
+		atomic.getAndIncrement();
+		System.out.println(atomic);
+		
 	}
 }
