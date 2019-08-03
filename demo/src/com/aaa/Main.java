b68c8ee533911f4e9883c58c3e4f6ebdac48ac7d
qinyue530 <530495973@qq.com>
2019-08-03 10:33:39 +0800

    试用贮藏功能

--- demo/src/com/aaa/Main.java
+++ demo/src/com/aaa/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("=================");
 		int n = sc.nextInt();
 		int w[] = new int[n];
 		int v[] = new int[n];
