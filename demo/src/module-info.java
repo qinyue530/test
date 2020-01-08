bedaf3bbe44c3ada5e07e19aa865bb1c83ae93ad
qinyue530 <530495973@qq.com>
2020-01-08 11:15:39 +0800

    修改JDK版本为 jdk13

---
+++ demo/src/module-info.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+/**
+ * @author Qin_Yue
+ *
+ */
+module test {
+	exports com.base.thread.art;
+	exports com.base.tree;
+	exports com.base.testThread;
+	exports com.designPattern.ProxyStatic;
+	exports com.base.ext;
+	exports com.designPattern.FactoryAbstract;
+	exports com.http.kill;
+	exports com.designPattern.FactorySimple;
+	exports com.juc.Thread;
+	exports com.designPattern.FactoryMethod;
+	exports com.base.io;
+	exports com.designPattern.Single;
+	exports com.base.Reflection;
+	exports com.designPattern.ProxyDynamic;
+	exports com.base.Algorithm;
+	exports com.aaa;
+	exports com.base.StaticAndFinal;
+	exports com.base.AbstractAndInterFace;
+	exports com.base.Integer;
+	exports com.socket.testTCP;
+	exports com.socket.testUDP;
+}
